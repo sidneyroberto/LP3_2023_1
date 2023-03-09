719f06c4a6d0e745f6d0e296482b3410597fd46d
Sidney Sousa <sidney.sousa@ifms.edu.br>
2023-03-08 21:48:21 -0400

    endpoint para salvar contato

--- exemplo01/src/routes/contacts.ts
+++ exemplo01/src/routes/contacts.ts
@@ -6,9 +6,16 @@ const contactCtrl = new ContactController()
 
 /**
  * O código abaixo pode apresentar problema de
- * binding (ligação/clonagem de funções)
- *
+ * binding (ligação/clonagem de funções), pois é
+ * feita apenas uma cópia do método save, sem a
+ * cópia completa do objeto contactCtrl:
  *
  * contactsRouter.post('/', contactCtrl.save)
+ *
+ * Para resolver este problema, alterei o código
+ * acima para este código que está em execução.
+ * Alternativamente, é possível realizar o binding
+ * explícito do objeto contactCtrl:
+ * contactsRouter.post('/', contactCtrl.save.bind(contactCtrl))
  */
 contactsRouter.post('/', (req, res) => contactCtrl.save(req, res))
